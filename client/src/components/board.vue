288bc585505dbf4e0fa4613ab78e7ed76e14ffc1
CalumCannon <calbo90@googlemail.com>
2019-05-09 16:28:49 +0100

    adds components

---
+++ client/src/components/board.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div class="">
+    <p>BOARD COMPONENT</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'board'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ client/src/views/gameview.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+<div class="">
+<board/>
+</div>
+</template>
+
+<script>
+import Board from "@/components/board.vue";
+export default {
+  components:{
+    "board": Board
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
