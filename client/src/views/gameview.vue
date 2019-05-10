101ca03c85830219d978d674a096d14a0a53852c
sridevi <sdara20@gmail.com>
2019-05-10 16:47:35 +0100

    creates dice componenet

--- client/src/views/gameview.vue
+++ client/src/views/gameview.vue
@@ -1,14 +1,17 @@
 <template lang="html">
 <div class="">
 <board/>
+<dice/>
 </div>
 </template>
 
 <script>
 import Board from "@/components/board.vue";
+import Dice from "@/components/dice.vue";
 export default {
   components:{
-    "board": Board
+    "board": Board,
+    "dice" : Dice
   }
 }
 </script>
