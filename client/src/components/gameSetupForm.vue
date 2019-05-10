7652902dc3a78c15e4b8dd8861bb198bd91292c2
CalumCannon <calbo90@googlemail.com>
2019-05-10 14:06:53 +0100

    template

---
+++ client/src/components/gameSetupForm.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+<div class="">
+  <form class="" action="index.html" method="post">
+
+  </form>
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
