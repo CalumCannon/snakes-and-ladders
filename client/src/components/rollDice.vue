d9a758cdde97fb1f018a8827687581d61ba01faa
sridevi <sdara20@gmail.com>
2019-05-12 11:28:11 +0100

    Adding image to dice

--- client/src/components/rollDice.vue
+++ client/src/components/rollDice.vue
@@ -2,7 +2,8 @@
  <div class="rolldice">
    <div id="dice" class="dice"></div>
    <div id="button">
-   <button id="roll" type="button"  v-on:click="diceRoll(1, 6)">Roll</button>
+   <button id="roll" type="button" value="http://clipartmag.com/images/dice-images-1-6-11.jpg" v-on:click="diceRoll(1, 6)">
+          <img src="http://clipartmag.com/images/dice-images-1-6-11.jpg "height="170" width="180"></button>
  </div>
   <!-- here the status holds the result of the roll random number -->
    <h2 id="status" style="clear:right;"></h2>
@@ -50,17 +51,17 @@ div.dice{
   color: blue;
 }
 #button{
-  float :center;
+  /* float :center;
   width: 32px;
   border:yellow 1px solid;
   padding: 10px;
   font-size: 24px;
   text-align: center;
-  margin:5px;
-  background-color: green;
-  color: gold yellow;
-  align-content: center;
-  background-image: url('https://www.shutterstock.com/image-vector/casino-dice-icon-on-black-background-1341070715?src=wDS4QVRCYgxUdv3tIc0Siw-1-0');
+  margin:5px;  */
+  /* background-color: green; */
+  /* color: gold yellow; */
+  /* align-content: center;
+   /* background-image: url('https://www.shutterstock.com/image-vector/casino-dice-icon-on-black-background-1341070715?src=wDS4QVRCYgxUdv3tIc0Siw-1-0'); */
 
 }
 </style>
