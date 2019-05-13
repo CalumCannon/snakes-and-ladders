695a2efa60c92e7f11c970cd44af7b1c3a25053e
annemck <annekh88@gmail.com>
2019-05-13 11:33:02 +0100

    select working, remove-selected not

--- client/src/components/gameSetupForm.vue
+++ client/src/components/gameSetupForm.vue
@@ -1,35 +1,50 @@
 <template lang="html">
   <div>
     
-    <form action="index.html" method="post">
-      
-      <label for="player-select">Select Player One</label>
-      <select v-model="playerOne">
-        <option selected disabled>Player One</option>
-        <option v-for="(player, index) in players" :value="player">
-          {{player.name}}
-        </option>
-        
-      </select>
-      <label for="playerTwo-select">Select Player Two</label>
-      <select v-model="playerTwo">
-        <option selected disabled>Player Two</option>
-        <option v-for="(player, index) in players" :value="player">
-          {{player.name}}
-        </option>
-      </select>
-      
-    </form>
+    <!-- <form action="index.html" method="post">
     
-    <router-link :to="{ name: 'game-view', params: {chosenPlayers} }">START GAME</router-link>
-    
-  </div>
+    <label for="player-select">Select Player One</label>
+    <select v-model="playerOne">
+    <option selected disabled>Player One</option>
+    <option v-for="(player, index) in players" :value="player">
+    {{player.name}}
+  </option>
+  
+</select>
+<label for="playerTwo-select">Select Player Two</label>
+<select v-model="playerTwo">
+<option selected disabled>Player Two</option>
+<option v-for="(player, index) in players" :value="player">
+{{player.name}}
+</option>
+</select>
+
+</form> -->
+
+  <label for="">Choose Players</label>
+    <multiselect
+    v-model="chosenPlayers"
+    :options="databasePlayers"
+    :multiple="true"
+    :close-on-select="false"
+    :clear-on-select="false"
+    label="name"
+    :hide-selected="true"
+    :max="6">
+    <template slot="option" slot-scope="props">
+      <img :src="props.option.avatar">{{props.option.name}}</template>
+    </multiselect>
+
+<router-link :to="{ name: 'game-view', params: {chosenPlayers} }">START GAME</router-link>
+
+</div>
 </template>
 
 // <script>
 import { eventBus } from '@/main.js';
 import GameService from '@/services/gameService.js';
 import GameView from '@/views/gameview.vue';
+import Multiselect from '@/main.js';
 
 export default {
   name: 'game-setup-form',
@@ -60,5 +75,6 @@ export default {
 }
 </script>
 
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 <style lang="css" scoped>
 </style>
