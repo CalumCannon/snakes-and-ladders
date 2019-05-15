a5c4dbdeb346e9dc5319dbdb39c4693adcf19704
CalumCannon <calbo90@googlemail.com>
2019-05-15 13:35:37 +0100

    stuff

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -8,16 +8,4 @@ db.players.insertMany([ // NEW
     losses: 0,
     avatar: "https://img.icons8.com/color/48/000000/guest-male.png"
   },
-  {
-    name: "Sri",
-    wins: 0,
-    losses: 0,
-    avatar: "https://img.icons8.com/color/48/000000/guest-male.png"
-  },
-  {
-    name: "Anne",
-    wins: 0,
-    losses: 0,
-    avatar: "https://img.icons8.com/color/48/000000/guest-male.png"
-  },
 ]);
