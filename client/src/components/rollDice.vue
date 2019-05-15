917a97fd4008be678181b66c9672beb48539539e
annemck <annekh88@gmail.com>
2019-05-15 11:30:02 +0100

    tidied up dice images

--- client/src/components/rollDice.vue
+++ client/src/components/rollDice.vue
@@ -4,7 +4,7 @@
     <div id="button">
       
       <button id="roll" type="button" v-on:click="diceRoll(1, 6)" :disabled="this.disabled"   v-bind:class="{'disable' : this.disabled}">
-        <canvas style="z-index: 10;" id="canvas" width="180" height="170"></canvas>
+        <img :src="checkImage(diceNumber)" alt="">
         </button>
         
       </div>
@@ -38,57 +38,45 @@
         eventBus.$emit('dice-rolled', randomNum);
         this.diceNumber = randomNum;
         console.log('diceRoll generated diceNumber');
-        // this.checkImage(this.diceNumber)
       },
       
       checkImage(number){
-        const image = new Image(180, 170)
-        const canvas = document.querySelector('#canvas')
-        const ctx = canvas.getContext('2d')
-        console.log('checkImag e started');
-        console.log('canvas', canvas);
-        console.log('ctx', ctx);
-        // ctx.drawImage(image, 0,0)
-        // ctx.fillStyle = 'black'
-        // ctx.font = 'Arial'
-        // ctx .fillText('hello!', 0, 0)
-        // ctx.clearRect(0, 0, canvas.width, canvas.height)
         switch(number){
           case 1:
-          image.src = "../assets/dice1.jpg"
-          ctx.drawImage(image, 0,0)
+          return reqiure("@/assets/dice1.jpg")
+          
           console.log('rolled 1');
           break
           case 2:
-          image.src = "../assets/dice2.jpg"
-          ctx.drawImage(image, 0,0)
+          return require("@/assets/dice2.jpg")
+        
           console.log('rolled 2');
           break
           case 3:
-          image.src = "../assets/dice3.jpg"
-          ctx.drawImage(image, 0,0)
+          return require("@/assets/dice3.jpg")
+        
           console.log('rolled 3');
           break
           case 4:
           console.log('rolled 4');
-          image.src = "../assets/dice4.jpg"
-          ctx.drawImage(image, 0,0)
+          return require("@/assets/dice4.jpg")
+          
           break
           case 5:
-          image.src = "../assets/dice5.jpg"
-          ctx.drawImage(image, 0,0)
+          return require("@/assets/dice5.jpg")
+      
           console.log('rolled 5');
           break
           case 6:
-          image.src = "../assets/dice6.jpg"
-          ctx.drawImage(image, 0,0)
+          return require("@/assets/dice6.jpg")
+        
           console.log('rolled 6');
           break
           default:
-          image.src = "http://clipartmag.com/images/dice-images-1-6-11.jpg"
-          ctx.drawImage(image, 0,0)
-          console.log('hey the defeault img', image);
-          // ctx.drawImage('../assets/dice6.jpg', 0,0)
+          return require("@/assets/dice.jpg")
+          
+        
+
           break
       }
       console.log('switch finished');
@@ -145,7 +133,13 @@
     }
     
     img:click {
-      animation: shake 1s;
+      animation: shake 3s;
+    }
+    
+    img{
+      margin-top: 30px;
+      margin-left: 30px;
+      width: 150px;
     }
     
   </style>
